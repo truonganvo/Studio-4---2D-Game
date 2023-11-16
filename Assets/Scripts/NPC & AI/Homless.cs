030c97c03d1545fc19414273f1fc3759cc6b260a
KhamHuyLu <85721895+KhamHuyLu@users.noreply.github.com>
2023-11-16 15:15:09 +1100

    update park

--- Assets/Scripts/NPC & AI/Homless.cs
+++ Assets/Scripts/NPC & AI/Homless.cs
@@ -1,25 +1,33 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Homeless : MonoBehaviour
 {
     [SerializeField] private WorldState worldState; // Assign this in the Inspector
     [SerializeField] private TextMeshProUGUI dialogueText; // Assign this in the Inspector
+    [SerializeField] private float moveSpeed = 2f; // Speed at which the homeless character moves
+    [SerializeField] private Transform moveTarget; // The target position to move to after being fed twice
+    [SerializeField] private string newSceneName; // The name of the new scene to load
 
     private bool isPlayerInRange = false;
 
     private void Start()
     {
-        // You could initialize dialogueText with an initial value if needed.
         UpdateDialogue();
     }
 
     private void Update()
     {
-        // If the player is in range and presses the 'E' key, give the item.
         if (isPlayerInRange && Input.GetKeyDown(KeyCode.E))
         {
             ReceiveItem();
+        }
+
+        // Check if the homeless character should move
+        if (worldState.foodGivenToHomeless >= 2 && moveTarget != null)
+        {
+            MoveToTarget();
         }
     }
 
@@ -52,7 +60,6 @@ public class Homeless : MonoBehaviour
                 break;
             case 2:
                 dialogueText.text = "I'm full now, thank you! What can I do for you?";
-                // Here you can also trigger any actions like moving to a new scene
                 break;
             default:
                 dialogueText.text = "You've already given me more than enough!";
@@ -70,6 +77,22 @@ public class Homeless : MonoBehaviour
         else
         {
             dialogueText.text = "It looks like you don't have any food to give.";
+        }
+    }
+
+    private void MoveToTarget()
+    {
+        // Calculate the direction to move
+        Vector3 moveDirection = (moveTarget.position - transform.position).normalized;
+
+        // Move the character towards the target
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+
+        // Check if the character has reached the target
+        if (Vector3.Distance(transform.position, moveTarget.position) < 0.1f)
+        {
+            // Load the new scene when the target is reached
+            SceneManager.LoadScene(newSceneName);
         }
     }
 }
